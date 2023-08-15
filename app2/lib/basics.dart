52b25675d5f8c10cd01c4f5ee9391b44b3dc87cf
gitadityakumar <ITSADITYASINGH05@GMAIL.COM>
2023-08-15 18:06:26 +0530

    indtroduction of Dart

---
+++ app2/lib/basics.dart
@@ -0,0 +1,16 @@
+// this is dart tutorial and i am going to learn it
+
+import 'dart:io';
+
+
+
+void main(){
+
+  stdout.write('Enter your Name:');
+  var name = stdin.readLineSync();
+  print("welcome, $name");
+
+  //time stamp 4:19:00  yaha se padhna start karo
+
+
+}
